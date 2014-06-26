b705626f9f7bee2a6f2bc960fe0551b30544de9b
Anthony Short <antshort@gmail.com>
2014-06-26 10:27:29 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
+var frame = document.createElement('iframe');
+frame.style.border = "none";
+frame.style.padding = "0";
+frame.style.margin = "0";
+frame.style.display = "none";
+document.body.appendChild(frame);
+
 module.exports = function(el, query) {
-  if(!exports.frame) {
-    var frame = document.createElement('iframe');
-    frame.style.border = "none";
-    frame.style.padding = "0";
-    frame.style.margin = "0";
-    frame.style.display = "none";
-    document.body.appendChild(frame);
-    exports.frame = frame;
-  }
-  exports.frame.style.width = el.clientWidth + 'px';
-  exports.frame.style.height = el.clientHeight + 'px';
-  var mq = exports.frame.contentWindow.matchMedia(query);
+  frame.style.width = el.clientWidth + 'px';
+  frame.style.height = el.clientHeight + 'px';
+  var mq = frame.contentWindow.matchMedia(query);
   return mq.matches;
 };
